d6c74a51ff3f5a1e29c85661bf6f82138b13600f
mahfuzurrah <mahfuzuruiux@gmail.com>
2024-04-02 23:48:41 +0600

    Add

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,7 @@
 import { Faq } from "@/components/accordion/Faq";
 import { HeroBanner } from "@/components/banner/HereBanner";
 import { CardHoverEffect } from "@/components/card/CardHoverEffect";
-import { WorkCard } from "@/components/card/WorkCard";
-import Testimonial from "@/components/carosol/Testimonial";
-import { InfiniteMoving } from "@/components/movingImg/InfiniteMoving";
+import { Testimonial } from "@/components/carosol/Testimonial";
 import Image from "next/image";
 
 export default function Home() {
@@ -38,60 +36,68 @@ export default function Home() {
       </section>
       <section className="py-[50px] bg" id="benefits">
         <div className="container">
+          {/* 
           <div className="w-full sm:w-[550px] m-auto mb-5">
             <h2 className="text-[30px] sm:text-[36px] text-white text-center font-bold">
               “Join a community of industry leading professionals”
             </h2>
+          </div> */}
+          <div className="flex items-center gap-4">
+            <div className="text-center w-full p-[40px] rounded-xl bg-[#242732]">
+              <h4 className="text-[22px] font-semibold">
+                Unfair lending claims paid out
+              </h4>
+              <h2 className="text-[36px] font-bold mt-3">£2.3 million </h2>
+            </div>
+            <div className="text-center w-full p-[40px] rounded-xl bg-[#242732]">
+              <h4 className="text-[22px] font-semibold">
+              Payment arrangements in place
+              </h4>
+              <h2 className="text-[36px] font-bold mt-3">1600</h2>
+            </div>
+            <div className="text-center w-full p-[40px] rounded-xl bg-[#242732]">
+              <h4 className="text-[22px] font-semibold">
+              Donated to charity 
+              </h4>
+              <h2 className="text-[36px] font-bold mt-3">£305k</h2>
+            </div>
           </div>
         </div>
-        <InfiniteMoving />
       </section>
       <section className="py-[80px] relative overflow-hidden">
         <div className="container">
-          <div className="flex items-start justify-center w-full">
-            <div className="flex flex-row items-center justify-center absolute top-[50px] -z-10">
-              <h1 className="combined">Benefits</h1>
-              <h1 className="combined">Benefits</h1>
-            </div>
-            <div className="w-[550px] m-auto text-center">
-              <h2 className="text-[30px] sm:text-[36px] md:text-[56px] mt-[-15px] md:mt-0 text-white text-center font-bold">
-                Benefits
-              </h2>
-            </div>
+          <div className="w-[550px] m-auto text-center">
+            <h2 className="text-[30px] sm:text-[36px] md:text-[56px] mt-[-15px] md:mt-0 text-white text-center font-bold">
+              Benefits
+            </h2>
           </div>
           <CardHoverEffect />
         </div>
       </section>
       <section className="py-[80px] h-full relative bg overflow-hidden">
         <div className="container">
-          <div className="flex items-start justify-center w-full">
-            <div className="flex flex-row items-center justify-center absolute top-[50px] -z-1">
-              <h1 className="combined">Voices of clients</h1>
-              <h1 className="combined">Voices of clients</h1>
-            </div>
-            <div className="w-[550px] m-auto text-center">
-              <h2 className="text-[30px] sm:text-[36px] md:text-[56px] mt-[-15px] md:mt-0 text-white text-center font-bold">
-                Voices Of Clients
-              </h2>
-            </div>
+          <div className="w-[550px] m-auto text-center">
+            <h2 className="text-[30px] sm:text-[36px] md:text-[56px] mt-[-15px] md:mt-0 text-white text-center font-bold">
+              Voices Of Clients
+            </h2>
           </div>
-          <div className="w-full mt-5">
-            <Testimonial/>
+          <div className="w-full mt-5 flex flex-col items-center">
+            <Testimonial />
+            <button className="mt-5 relative inline-flex overflow-hidden rounded-full p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
+              <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
+              <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-6 py-3 text-sm font-medium text-white backdrop-blur-3xl">
+                Leave Your Review
+              </span>
+            </button>
           </div>
         </div>
       </section>
       <section className="py-[80px] relative overflow-hidden">
         <div className="container">
-          <div className="flex items-start justify-center w-full">
-            <div className="flex flex-row items-center justify-center absolute top-[50px] -z-10">
-              <h1 className="combined">Frequently asked questions</h1>
-              <h1 className="combined">Frequently asked questions</h1>
-            </div>
-            <div className="text-center">
-              <h2 className="text-[30px] sm:text-[36px] md:text-[56px] mt-[-15px] md:mt-0 text-white text-center font-bold">
-                HAVE QUESTIONS?
-              </h2>
-            </div>
+          <div className="text-center">
+            <h2 className="text-[30px] sm:text-[36px] md:text-[56px] mt-[-15px] md:mt-0 text-white text-center font-bold">
+              HAVE QUESTIONS?
+            </h2>
           </div>
           <div className="mt-8 z-10">
             <Faq />
--- src/components/ui/testimonial-moving-cards.tsx
+++ src/components/ui/testimonial-moving-cards.tsx
@@ -1,9 +1,8 @@
 "use client";
 import { cn } from "@/utils/cn";
-import Image from "next/image";
 import React, { useEffect, useState } from "react";
 
-export const InfiniteMovingCards = ({
+export const TestimonialMovingCards = ({
   items,
   direction = "left",
   speed = "fast",
@@ -11,7 +10,9 @@ export const InfiniteMovingCards = ({
   className,
 }: {
   items: {
-    bimg: string;
+    quote: string;
+    name: string;
+    title: string;
   }[];
   direction?: "left" | "right";
   speed?: "fast" | "normal" | "slow";
@@ -78,20 +79,34 @@ export const InfiniteMovingCards = ({
       <ul
         ref={scrollerRef}
         className={cn(
-          " flex min-w-full shrink-0 gap-20 py-4 w-max flex-nowrap",
+          " flex min-w-full shrink-0 gap-4 py-4 w-max flex-nowrap",
           start && "animate-scroll ",
           pauseOnHover && "hover:[animation-play-state:paused]"
         )}
       >
         {items.map((item, idx) => (
-          <li className="w-[100%] relative" key={idx}>
+          <li
+            className="w-[350px] max-w-full relative rounded-2xl border border-b-0 flex-shrink-0 border-slate-700 px-8 py-6 md:w-[450px] bg-[#242732]"
+            key={item.name}
+          >
             <blockquote>
-                <Image
-                  width={200}
-                  height={200}
-                  src={item.bimg}
-                  alt={`testimonial-${idx}`}
-                />
+              <div
+                aria-hidden="true"
+                className="user-select-none -z-1 pointer-events-none absolute -left-0.5 -top-0.5 h-[calc(100%_+_4px)] w-[calc(100%_+_4px)]"
+              ></div>
+              <span className="relative z-20 leading-[1.6] text-gray-100 text-[16px] font-medium">
+                {item.quote}
+              </span>
+              <div className="relative z-20 mt-6 flex flex-row items-center">
+                <span className="flex flex-col gap-1">
+                  <span className=" text-[18px] leading-[1.6] text-white font-bold ">
+                    {item.name}
+                  </span>
+                  <span className=" text-sm leading-[1.6] text-gray-400 font-normal">
+                    {item.title}
+                  </span>
+                </span>
+              </div>
             </blockquote>
           </li>
         ))}
